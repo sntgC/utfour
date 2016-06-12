080a1b01daae7548cc4268320f71aa775c2b9689
Santiago <sntgcortes23@gmail.com>
2016-06-12 17:20:05 -0500

    Added a test page used to display information from the database for future test purposes. Also added a php page that sets the winner of a game.

---
+++ setWinner.php
@@ -0,0 +1,16 @@
+<?php
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "ut4serverdb";
+	$connect = new mysqli($server,$username,$password,$database) or die("Failed to connect to the server");
+	$sql = "UPDATE games SET winnerID = '$_GET[winID]' WHERE id='$_GET[gameID]'";
+	$result = mysqli_query($connect,$sql);
+	if (!$result){
+		die ("The SQL command was not processed correctly");
+	} else{
+		//Redirects to the page where the form was added. This can be avoided using AJAX
+		header( 'Location: /test.html' ) ;
+	}
+	mysqli_close($connect);
+?>
