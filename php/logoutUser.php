f8f4366858feae6a0cc917f54ad7889e544ecef7
CDogg99 <camd.99@gmail.com>
2016-06-18 00:07:56 -0500

    Added index.html and made other minor changes
    The basic website framework is now established and we must now begin
    integrating some of the more complex features.  I will do some finishing
    touches later on the registration verification and a better function to
    check for existing cookies.

--- php/logoutUser.php
+++ php/logoutUser.php
@@ -2,6 +2,5 @@
 	$cookie_name="username";
 	unset($_COOKIE[$cookie_name]);
 	setcookie($cookie_name,"",time() - (86400*366),"/");
-	//Temporary redirect. Will be set to index.html later on.
-	header("Location: ../lobby.html");
+	header("Location: ../index.html");
 ?>
