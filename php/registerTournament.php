37b95228d86ff399eb0a30d0b29ff2e80f9d9b30
Santiago <sntgcortes23@gmail.com>
2016-07-01 17:26:41 -0500

    Added registerTournament.php

---
+++ php/registerTournament.php
@@ -0,0 +1,20 @@
+<?php
+	$server="localhost";
+	$username="root";
+	$password="";
+	$database="ut4serverdb";
+	
+	$connection=mysqli_connect($server,$username,$password) or die("Failed to connect to the server");
+	mysqli_select_db($connection,$database) or die("Failed to connect to the database");
+	/* $¢ Change this to POST when you figure out how to do it with ajax */
+	$sql = "INSERT INTO tournaments (id, data) VALUES ('$_GET[tourID]','$_GET[tourData]')";
+
+	if ($connection->query($sql)===TRUE){
+		echo "Data succesfully inputted";
+	}else{
+		echo "Something seems to have gone wrong";
+	}
+	
+	$connection->close();
+	exit();
+?>
