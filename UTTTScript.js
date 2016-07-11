e38da2b0e99a748154e2763a76e1bcc6543b6eaa
Santiago <sntgcortes23@gmail.com>
2016-07-11 12:52:47 -0500

    Began graphics work

--- UTTTScript.js
+++ UTTTScript.js
@@ -1,4 +1,8 @@
-var onPlay=9;
+//var onPlay=9;
+var c;
+var ctx;
+var colorA="#cc6699";
+var colorB="#ffff99";
 
 class MiniGrid{
 	
@@ -19,7 +23,7 @@ class MiniGrid{
 	isFull(){
         for(var mgr=0;mgr<3;mgr++)
             for(var mgc=0;mgc<3;mgc++)
-                if(this.cells[r][c]==='0')
+                if(this.cells[mgr][mgc]==='0')
                     return false;
         return true;
     }
@@ -68,8 +72,31 @@ class MiniGrid{
 		this.isWon();
 	}
 	
+	draw(){
+		var xIndex=this.drawData[0];
+        var yIndex=this.drawData[1];
+        for(var drawR=0;drawR<3;drawR++){
+            for(var drawC=0;drawC<3;drawC++){
+                switch(this.cells[drawR][drawC]){
+					case '0':
+						ctx.fillStyle="#dfdfdf";
+						break;
+					case '1':
+						ctx.fillStyle=colorA;
+						break;
+					case '2':
+						ctx.fillStyle=colorB;
+				}
+				ctx.fillRect(xIndex,yIndex,this.drawData[2]*3,this.drawData[2]*3);
+				 xIndex+=this.drawData[2]*4;
+            }
+            xIndex=this.drawData[0];
+            yIndex+=this.drawData[2]*4;
+        }
+	}
+	
 	toString(){
-		var ret="";
+		var ret=""+this.drawData[0]+","+this.drawData[1]+" "+this.drawData[2]+"\n";
 		for(var mgr=0;mgr<3;mgr++){
 			for(var mgc=0;mgc<3;mgc++)
 				ret+=this.cells[mgr][mgc];
@@ -81,32 +108,114 @@ class MiniGrid{
 
 class Grid {
 	constructor(x,y, scale) {
-		this.grid=x;
+		this.grid=[];
+		this.onPlay=9;
+		var xIndex=x;
+		var yIndex=y;
+		for(var gr=0;gr<3;gr++){
+			var tempGrid=[];
+			for(var gc=0;gc<3;gc++){
+				tempGrid.push(new MiniGrid(xIndex,yIndex,scale/39));
+                xIndex+=scale/39*14;
+			}
+			this.grid.push(tempGrid);
+			xIndex=x;
+            yIndex+=scale/39*14;
+		}
 	}
+	
+	gameWon(){
+        for(var r=0;r<3;r++){
+            if(this.grid[r][0].getWinner()!='0'&&this.grid[r][0].getWinner()==this.grid[r][1].getWinner()&&this.grid[r][1].getWinner()==this.grid[r][2].getWinner())
+                return true;
+            if(this.grid[0][r].getWinner()!='0'&&this.grid[0][r].getWinner()==this.grid[1][r].getWinner()&&this.grid[1][r].getWinner()==this.grid[2][r].getWinner())
+                return true;
+        }
+        if(this.grid[0][0].getWinner()!='0'&&this.grid[0][0].getWinner()==this.grid[1][1].getWinner()&&this.grid[1][1].getWinner()==this.grid[2][2].getWinner())
+            return true;
+        if(this.grid[0][2].getWinner()!='0'&&this.grid[0][2].getWinner()==this.grid[1][1].getWinner()&&this.grid[1][1].getWinner()==this.grid[2][0].getWinner())
+            return true;
+        return false;
+    }
+	
+	 setCell(coords,car){
+        if(this.onPlay===9&&!this.grid[coords[0]][coords[1]].isWon()&&!this.grid[coords[0]][coords[1]].isFull()){
+            if(this.grid[coords[0]][coords[1]].setCell(coords[2],coords[3],car)){
+                this.grid[coords[0]][coords[1]].isWon();
+                if(this.grid[coords[2]][coords[3]].isWon()||this.grid[coords[2]][coords[3]].isFull())
+                    this.onPlay=9;
+                else
+                    this.onPlay=coords[2]*3+coords[3];
+                return true;
+            }else
+                return false;
+        }else if(coords[0]*3+coords[1]===this.onPlay&&!this.grid[coords[0]][coords[1]].isWon()&&!this.grid[coords[0]][coords[1]].isFull()){
+            if(this.grid[coords[0]][coords[1]].setCell(coords[2],coords[3],car)){
+                this.grid[coords[0]][coords[1]].isWon();
+                if(this.grid[coords[2]][coords[3]].isWon()||this.grid[coords[2]][coords[3]].isFull())
+                    this.onPlay=9;
+                else
+                    this.onPlay=coords[2]*3+coords[3];
+                return true;
+            }else
+                return false;
+        }
+        return false;
+    }
+	
+	getCoords(x,y){
+		
+	}
+	
+	encode(){
+		
+	}
+	
+	decode(gameData){
+		
+	}
+	
+	draw(){
+		for(var dr=0;dr<3;dr++){
+			for(var dc=0;dc<3;dc++)
+				this.grid[dr][dc].draw();
+		}
+	}
+	
 	toString(){
-		return "";
+		var ret="";
+		for(var gr=0;gr<3;gr++){
+			for(var gc=0;gc<3;gc++)
+				ret+=this.grid[gr][gc];
+			ret+="\n";
+		}
+		return ret;	
 	}
 }
 
-
-function encode(gameData){
-
-}
-
-function decode(gameData){
-
-}
 
 function draw(){
-	
+	ctx.fillRect(0,0,20,50);
 }
 
-var miniBoard=new MiniGrid(1,2,3);
+/*var miniBoard=new MiniGrid(1,2,3);
+var board=new Grid(0,0,156);
+console.log(board.toString());
+console.log(board.setCell([0,0,0,0],"1"));
+console.log(board.setCell([0,0,0,0],"1"));
+console.log(board.toString());
 /*console.log(miniBoard.setCell(0,0,1));
 console.log(miniBoard.setCell(0,0,1));
 console.log(miniBoard.setCell(1,1,1));
 console.log(miniBoard.setCell(2,2,1));
-console.log(miniBoard.setCell(0,2,2));*/
+console.log(miniBoard.setCell(0,2,2));
 miniBoard.loadGrid("011021001");
 console.log(miniBoard.toString());
-console.log(miniBoard.isWon());
+console.log(miniBoard.isWon());*/
+
+jQuery(document).ready(function(){
+	var board=new Grid(0,0,156);
+	c=document.getElementById("display");
+	ctx=c.getContext("2d");
+	board.draw();
+});
