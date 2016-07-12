f5539fcbeb4a2714c4647bac34ca3cab99e72b5d
Cameron <camd.99@gmail.com>
2016-07-12 14:24:37 -0500

    Set the default character set of the database and tables to utf8

--- php/firstTimeSetUp.php
+++ php/firstTimeSetUp.php
@@ -5,24 +5,24 @@
 	$database = "ut4serverdb";
 	
 	$connect = new mysqli($server,$username,$password) or die("Failed to connect to the server");
-	$createDB = "CREATE DATABASE IF NOT EXISTS ut4serverdb; ";
+	$createDB = "CREATE DATABASE IF NOT EXISTS ut4serverdb CHARACTER SET utf8; ";
 	$connect->query($createDB);
 	
 	mysqli_select_db($connect,$database) or die("Failed to connect to the database");
 	
 	$sql = "";
-	$sql .="CREATE TABLE IF NOT EXISTS tournaments (
+	$sql .="CREATE TABLE IF NOT EXISTS tournaments(
 				id varchar(8) CHARACTER SET utf8 NOT NULL,
 				data varchar(1024) CHARACTER SET utf8 NOT NULL
-				); ";
-	$sql .="CREATE TABLE IF NOT EXISTS games (
+				) CHARACTER SET utf8; ";
+	$sql .="CREATE TABLE IF NOT EXISTS games(
 				id varchar(8) CHARACTER SET utf8 DEFAULT NULL,
 				player1ID varchar(8) CHARACTER SET utf8 DEFAULT NULL,
 				player2ID varchar(8) CHARACTER SET utf8 DEFAULT NULL,
 				winnerID varchar(8) CHARACTER SET utf8 DEFAULT NULL,
 				gameData varchar(1024) CHARACTER SET utf8 DEFAULT '19000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-				); ";
-	$sql .="CREATE TABLE IF NOT EXISTS users (
+				) CHARACTER SET utf8; ";
+	$sql .="CREATE TABLE IF NOT EXISTS users(
 				userID varchar(7) CHARACTER SET utf8 NOT NULL,
 				username varchar(20) CHARACTER SET utf8 DEFAULT NULL,
 				password varchar(20) CHARACTER SET utf8 DEFAULT NULL,
@@ -30,12 +30,12 @@
 				wins int NOT NULL DEFAULT 0,
 				beekeeper boolean NOT NULL DEFAULT 0,
 				PRIMARY KEY (userID)
-				); ";
-	$sql .="CREATE TABLE IF NOT EXISTS lobby ( 
+				) CHARACTER SET utf8; ";
+	$sql .="CREATE TABLE IF NOT EXISTS lobby( 
 				userID varchar(7) CHARACTER SET utf8 NOT NULL,
 				lastActive timestamp NOT NULL,
 				PRIMARY KEY (userID)	
-				); ";
+				) CHARACTER SET utf8; ";
 				
 	$result = mysqli_multi_query($connect,$sql);
 	if (!$result){
