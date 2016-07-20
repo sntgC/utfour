c8ae0cd62c52d02ccc5459c07b665cdfa73a5949
Cameron <camd.99@gmail.com>
2016-07-20 17:21:28 -0500

    Set menu min-width to 0px

--- lobby.php
+++ lobby.php
@@ -5,7 +5,6 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link rel="stylesheet" type="text/css" href="style/header.css">
-		<!--<link rel="stylesheet" type="text/css" href="style/header.css">-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 		<script type="text/javascript" src="script.js"></script>
 		<script type="text/javascript">
