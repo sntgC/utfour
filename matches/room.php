d31e9160d0c341145e41e1747fb0a467d41c0325
Cameron <camd.99@gmail.com>
2016-09-01 20:54:35 -0500

    Corrected favicon link in room.php

--- matches/room.php
+++ matches/room.php
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" type="text/css" href="../style/header.css">
 		<link rel="stylesheet" type="text/css" href="../style/fontello.css">
 		<link rel="stylesheet" type="text/css" href="../style/room.css">
-		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+		<link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 		<script type="text/javascript" src="../script.js"></script>
 		<script type="text/javascript" src="../roomScript.js"></script>
