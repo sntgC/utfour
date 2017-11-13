71a37f15d0c3475b5686999ed751929ae0b6b58e
Santiago <sntgcortes23@gmail.com>
2017-11-12 22:27:29 -0600

    Styled email change page

--- changeEmail.php
+++ changeEmail.php
@@ -3,9 +3,9 @@
 	<head>
 		<title>Change Email Address</title>
 		<meta charset="utf-8">
-		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css" href="style/style.css">
-		<link rel="stylesheet" type="text/css" href="style/header.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="style/utfour.css">
+		<link rel="stylesheet" type="text/css" href="style/form.css">
 		<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 		<script type="text/javascript" src="script.js"></script>
@@ -24,6 +24,7 @@
 				document.getElementById("xEmail").style.display = "none";
 				document.getElementById("emailNotif").style.display = "none";
 				document.getElementById("emailConfNotif").style.display = "none";
+                $(".dropdown-content").toggle();
 			}
 			
 			var availableEmail=false;
@@ -146,7 +147,7 @@
 			// Close the dropdown if the user clicks outside of the button or image
 			window.onclick = function(e) {
 				//.dropdownLink is the class for anything that does not hide the dropdowns
-				if (!e.target.matches('#usrImg')&&!e.target.matches(".dropdownLink")) {
+				if (!e.target.matches('#usrImg')&&!e.target.matches(".dropdownLink")&&!e.target.matches(".dropbtn")) {
 					var keys=Object.keys(isOpen);
 					for(o=0;o<keys.length;o++){
 						if(isOpen[keys[o]]){
@@ -166,37 +167,42 @@
 		</script>
 	</head>
 	<body onload="init()" style="display:none">
-		<ul class="blue">
-			<li class="dropdown right" id="userData">
-				<a href="javascript:dropMenu('accountSettings');" class="dropbtn dropdownLink blue"><?php include 'php/loadUserImg.php'; $emailOnly=""; $winsOnly=""; $includeWins="true"; include 'php/getUser.php';?></a>
+		<div class="header">
+			<div class="dropdown right" id="userData">
+				<a href="javascript:dropMenu('accountSettings');" class="dropbtn  blue"><?php include 'php/loadUserImg.php'; $emailOnly=""; $winsOnly=""; $includeWins="true"; include 'php/getUser.php';?></a>
 				<div class="dropdown-content" id="accountSettings">
 					<a href="spectate" class="dropdownLink">Spectate</a>
 					<a href="php/logoutUser.php" class="dropdownLink">Sign Out</a>
 				</div>
-			</li>
-			<li class="dropdown left">
-				<a href="lobby" class="dropbtn title blue">UT<sup>4</sup></a>
-			</li>
-			<li class="dropdown left">
+			</div>
+            <span class="filler"></span>
+			<div class="dropdown left">
 				<a href="account" class="dropbtn title blue">My Account</a>
-			</li>
-		</ul>
-		<h1 class="formSectionTitle">Change Email Address</h1>
-		<div class="formSection blue">
-			<p id="alert"></p>
-			<form onsubmit="return verifyForm()" action="php/updateEmail.php" method="post" name="changeEmailForm" id="changeEmailForm">
-				<label>Current Email Address</label><br>
-				<input class="blue" id="currEmail" type="text" name="currEmailIn" title="Please enter your current email."><br>
-				<label>New Email Address</label><br>
-				<input class="blue shortInput" id="email" type="text" name="emailIn" oninput="checkEmail(this.value)" title="Please enter a valid email address that is no more than 100 characters in length and is not already in use.">
-				<img src="images/green_checkmark.png" alt="Email available"  title="Email available" width="25" height="25" id="checkmarkEmail" class="smallImg">
-				<img src="images/red_x.png" alt="Email unavailable"  title="Email unavailable" width="25" height="25" id="xEmail" class="smallImg"><br>
-				<p id="emailNotif" class="warningText"></p>
-				<label>New Email Address Confirmation</label><br>
-				<input class="blue" id="emailConf" type="text" name="emailInConf" title="Please re-enter your new email address."><br>
-				<p id="emailConfNotif" class="warningText"></p>
-				<input class="blue" type="submit" id="submit" value="Change Email Address">
-			</form>
+			</div>
+			<div class="dropdown left">
+				<a href="lobby" class="dropbtn title blue">UT<sup>4</sup></a>
+			</div>
 		</div>
+        <div class="center-container">
+            <div class="form-container">
+                <h1 class="form-title">Change Email Address</h1>
+                <div class="formSection blue">
+                    <p id="alert"></p>
+                    <form onsubmit="return verifyForm()" action="php/updateEmail.php" method="post" name="changeEmailForm" id="changeEmailForm">
+                        <label class="form-label">Current Email Address</label><br>
+                        <input class="form-text-field" id="currEmail" type="text" name="currEmailIn" title="Please enter your current email."><br>
+                        <label class="form-label">New Email Address</label><br>
+                        <input class="form-text-field shortInput" id="email" type="text" name="emailIn" oninput="checkEmail(this.value)" title="Please enter a valid email address that is no more than 100 characters in length and is not already in use.">
+                        <span id="checkmarkEmail"><i class="material-icons green-txt">check</i></span>
+                        <span id="xEmail"><i class="material-icons red-txt">close</i></span><br>
+                        <p id="emailNotif" class="warningText"></p>
+                        <label class="form-label">New Email Address Confirmation</label><br>
+                        <input class="form-text-field" id="emailConf" type="text" name="emailInConf" title="Please re-enter your new email address."><br>
+                        <p id="emailConfNotif" class="warningText"></p>
+                        <input class="submit" type="submit" id="submit" value="Change Email Address">
+                    </form>
+                </div>
+            </div>
+        </div>
 	</body>
 </html>
